aba967be06a87d9867df9768c2fcaa2db5ffe20a
Kireeti <kireeti93@gmail.com>
2019-05-21 21:22:57 +0530

    Deleted project sample

---
+++ src/components/DynamicEditor.vue
@@ -0,0 +1,19 @@
+<style>
+#dynamic-editor {
+  flex: 1;
+}
+</style>
+
+<template>
+  <div id="dynamic-editor">
+    <div class="left-panel">Left panel</div>
+    <div class="editor-area">Editor area</div>
+    <div class="right-panel">Right panel</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DynamicEditor"
+};
+</script>

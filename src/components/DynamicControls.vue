c2e82ae72c2aa177e108763df362c24dcfc79670
Kireeti <kireeti93@gmail.com>
2019-05-22 12:14:05 +0530

    Working on DynamicControls

---
+++ src/components/DynamicControls.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="editor-controls">
+        <h4>Controls</h4>
+        <div v-if="selectedContainer && selectedContainer.children.length > 0">
+            <!-- childen list -->
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+        <div v-else>No items in the container</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DynamicControls",
+    props: ["selectedItem"],
+}
+</script>
+
+<style>
+
+</style>
--- src/components/DynamicEditor.vue
+++ src/components/DynamicEditor.vue
@@ -29,14 +29,7 @@
     <div id="dynamic-editor" v-on-clickaway="clickedAway">
         <dynamic-card class="left-panel">
             <div v-show="selectedContainer !== 'undefined'">
-                <h4>Controls</h4>
-                <div v-if="selectedContainer && selectedContainer.children.length > 0">
-                    <!-- childen list -->
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div>
-                <div v-else>No items in the container</div>
+                
             </div>
         </dynamic-card>
         <dynamic-card class="editor-area">

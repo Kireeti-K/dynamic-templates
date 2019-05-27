3ab1561a70b0d7c4b5e6e6e71b550deedfff5809
yeshwanth <shwnthr@gmail.com>
2019-05-27 15:37:05 +0530

    pulled new stuff from master

--- src/classes/TableCellContainer.js
+++ src/classes/TableCellContainer.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable import/prefer-default-export */
-import { Container } from '../internal';
-=======
-import Container from './Container';
-import TextElement from './TextElement';
->>>>>>> composer
+import { Container, TextElement } from '../internal';
 
 export class TableCellContainer extends Container {
     constructor() {
